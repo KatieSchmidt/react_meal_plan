9e6d706f8c99ecbc4189bfde4f92cd59e05ebdb1
Katie Schmidt <katie_schmidt7@icloud.com>
2018-09-29 12:43:16 -0400

    remove day schema and redo MealPlan Schema and create routes

--- models/MealPlan.js
+++ models/MealPlan.js
@@ -8,34 +8,14 @@ const MealPlanSchema = new Schema({
     type: String,
     required: true
   },
-  monday: {
-    type: Schema.Types.ObjectId,
-    ref: "day"
-  },
-  tuesday: {
-    type: Schema.Types.ObjectId,
-    ref: "day"
-  },
-  wednesday: {
-    type: Schema.Types.ObjectId,
-    ref: "day"
-  },
-  thursday: {
-    type: Schema.Types.ObjectId,
-    ref: "day"
-  },
-  friday: {
-    type: Schema.Types.ObjectId,
-    ref: "day"
-  },
-  saturday: {
-    type: Schema.Types.ObjectId,
-    ref: "day"
-  },
-  sunday: {
-    type: Schema.Types.ObjectId,
-    ref: "day"
-  }
+  meals: [
+    {
+      meal: {
+        type: Schema.Types.ObjectId,
+        ref: "meal"
+      }
+    }
+  ]
 });
 
 module.exports = MealPlan = mongoose.model("mealplans", MealPlanSchema);
--- routes/api/meal-plan.js
+++ routes/api/meal-plan.js
@@ -4,6 +4,7 @@ const Meal = require("../../models/Meal");
 const MealPlan = require("../../models/MealPlan");
 const Day = require("../../models/Day");
 
+//GET ROUTES
 //@route  GET api/meal-plan
 //@dsc    retrieve plans
 //@access Public
@@ -15,14 +16,60 @@ router.get("/", (req, res) => {
     );
 });
 
+//@route  GET api/meal-plan/:plan_id
+//@dsc    retrieve plan by id
+//@access Public
+router.get("/:plan_id", (req, res) => {
+  MealPlan.findById(req.params.plan_id)
+    .then(plan => res.json(plan))
+    .catch(err =>
+      res
+        .status(404)
+        .json({ noMealPlanFound: "No meal plan found with that id" })
+    );
+});
+
+//POST ROUTES
 //@route  POST api/meal-plan
 //@dsc    create mealplan
 //@access Public
 router.post("/", (req, res) => {
-  const newPlan = new MealPlan({
+  const plan = new MealPlan({
     planname: req.body.planname
   });
-  newPlan.save().then(plan => res.json(plan));
+  plan
+    .save()
+    .then(plan => res.json(plan))
+    .catch(err => res.json({ saveError: "unable to save mealplan" }));
+});
+
+//@route  POST api/meal-plan/:plan_id
+//@dsc    add meal to mealplan
+//@access Public
+router.post("/:plan_id", (req, res) => {
+  MealPlan.findById(req.params.plan_id).then(mealplan => {
+    Meal.findById(req.body.mealid).then(meal => {
+      mealplan.meals.unshift(meal);
+      mealplan.save().then(mealplan => res.json(mealplan));
+    });
+  });
+});
+
+//DELETE ROUTES
+
+//@route  DELETE api/meal-plan/:plan_id
+//@dsc    delete plan by id
+//@access Public
+router.delete("/:plan_id", (req, res) => {
+  MealPlan.findByIdAndRemove(req.params.plan_id)
+    .then(() => {
+      res.json({ success: true });
+    })
+    .catch(err =>
+      res
+        .status(404)
+        .json({ noMealPlanFound: "No meal plan found with that id" })
+    );
 });
 
 module.exports = router;
