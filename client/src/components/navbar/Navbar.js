87a4c87018b1cbd190228d42074e3995e78bc83e
Katie Schmidt <katie_schmidt7@icloud.com>
2019-03-28 08:56:34 -0400

    update npm packages

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -15,11 +15,7 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <a
-            href=""
-            onClick={this.onLogoutClick.bind(this)}
-            className="nav-link"
-          >
+          <a onClick={this.onLogoutClick.bind(this)} className="nav-link">
             Logout
           </a>
         </li>
